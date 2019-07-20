cb6703428d2c7fcb2d69858de5b7d0de72df8bda
N7Aakash <t2lassi12345@gmail.com>
2019-07-20 20:09:25 +0530

    Akash commit

--- index.php
+++ index.php
@@ -68,103 +68,21 @@
   </head>
   <body class="hold-transition skin-blue sidebar-mini">
     <div class="wrapper">
-      <header class="main-header">
-        <!-- Logo -->
-        <a href="index2.html" class="logo">
-          <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>A</b>LT</span>
-          <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Admin</b>LTE</span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top">
-          <!-- Sidebar toggle button-->
-          <a
-            href="#"
-            class="sidebar-toggle"
-            data-toggle="push-menu"
-            role="button"
-          >
-            <span class="sr-only">Toggle navigation</span>
-          </a>
+      
 
-          <div class="navbar-custom-menu">
-            <ul class="nav navbar-nav">
-              <!-- Messages: style can be found in dropdown.less-->
-              <li class="dropdown messages-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <i class="fa fa-envelope-o"></i>
-                  <span class="label label-success">4</span>
-                </a>
-              </li>
-              <!-- Notifications: style can be found in dropdown.less -->
-              <li class="dropdown notifications-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <i class="fa fa-bell-o"></i>
-                  <span class="label label-warning">10</span>
-                </a>
-              </li>
-              <!-- Tasks: style can be found in dropdown.less -->
-              <li class="dropdown tasks-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <i class="fa fa-flag-o"></i>
-                  <span class="label label-danger">9</span>
-                </a>
-              </li>
-              <!-- User Account: style can be found in dropdown.less -->
-              <li class="dropdown user user-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <img
-                    src="dist/img/user2-160x160.jpg"
-                    class="user-image"
-                    alt="User Image"
-                  />
-                  <span class="hidden-xs">Alexander Pierce</span>
-                </a>
-              </li>
-              <!-- Control Sidebar Toggle Button -->
-            </ul>
-          </div>
-        </nav>
-      </header>
+        <!-- header goes here -->
+        <?php
+        include_once ("includes/templates/header.php");
+        ?>
+        <!-- header ends -->
+
+
       <!-- Left side column. contains the logo and sidebar -->
-      <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-          <!-- Sidebar user panel -->
-          <div class="user-panel">
-            <div class="pull-left image">
-              <img
-                src="dist/img/user2-160x160.jpg"
-                class="img-circle"
-                alt="User Image"
-              />
-            </div>
-            <div class="pull-left info">
-              <p>Alexander Pierce</p>
-            </div>
-          </div>
-
-          <!-- sidebar menu: : style can be found in sidebar.less -->
-          <ul class="sidebar-menu" data-widget="tree">
-            <li class="header">MAIN NAVIGATION</li>
-            <li class="active treeview">
-              <a href="#">
-                <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-              </a>
-            </li>
-
-            <li class="header">LABELS</li>
-            <li>
-              <a href="#"
-                ><i class="fa fa-circle-o text-red"></i>
-                <span>Important</span></a
-              >
-            </li>
-          </ul>
-        </section>
-        <!-- /.sidebar -->
-      </aside>
+      <!-- side bar goes ehre -->
+      <?php
+      include_once ("includes/templates/navbar.php");
+      ?>
+      <!-- side bar ends -->
 
       <!-- Content Wrapper. Contains page content -->
       <div class="content-wrapper">
@@ -181,15 +99,22 @@
             <li class="active">Dashboard</li>
           </ol>
         </section>
+
+
+        <!-- BODY GOES HERE -->
+        
+        <!-- BODY ENDS HERE -->
+
+
       </div>
       <!-- /.content-wrapper -->
-      <footer class="main-footer">
-        <div class="pull-right hidden-xs"><b>Version</b> 0.0.1</div>
-        <strong
-          >Copyright &copy; 2019-2020          <a href="https://adminlte.io">CFG TEAM 7</a>.</strong
-        >
-        All rights reserved.
-      </footer>
+      
+
+        <!-- Footer start -->
+        <?php
+        include_once("includes/footer.php");
+        ?>
+        <!-- Footer end -->
     </div>
     <!-- ./wrapper -->
 
