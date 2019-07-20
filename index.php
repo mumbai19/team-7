99561987672245d69464e3477450c3b9f63a7a65
N7Aakash <t2lassi12345@gmail.com>
2019-07-20 17:37:22 +0530

    ui files

--- index.php
+++ index.php
@@ -12,23 +12,52 @@
     <!-- Bootstrap 3.3.7 -->
     <link
       rel="stylesheet"
-      href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css"
+      href="bower_components/bootstrap/dist/css/bootstrap.min.css"
     />
     <!-- Font Awesome -->
     <link
       rel="stylesheet"
-      href="assets/bower_components/font-awesome/css/font-awesome.min.css"
+      href="bower_components/font-awesome/css/font-awesome.min.css"
     />
     <!-- Ionicons -->
     <link
       rel="stylesheet"
-      href="assets/bower_components/Ionicons/css/ionicons.min.css"
+      href="bower_components/Ionicons/css/ionicons.min.css"
     />
     <!-- Theme style -->
-    <link rel="stylesheet" href="assets/dist/css/AdminLTE.min.css" />
+    <link rel="stylesheet" href="dist/css/AdminLTE.min.css" />
     <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="assets/dist/css/skins/_all-skins.min.css" />
+    <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css" />
+    <!-- Morris chart -->
+    <link rel="stylesheet" href="bower_components/morris.js/morris.css" />
+    <!-- jvectormap -->
+    <link
+      rel="stylesheet"
+      href="bower_components/jvectormap/jquery-jvectormap.css"
+    />
+    <!-- Date Picker -->
+    <link
+      rel="stylesheet"
+      href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css"
+    />
+    <!-- Daterange picker -->
+    <link
+      rel="stylesheet"
+      href="bower_components/bootstrap-daterangepicker/daterangepicker.css"
+    />
+    <!-- bootstrap wysihtml5 - text editor -->
+    <link
+      rel="stylesheet"
+      href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css"
+    />
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
     <!-- Google Font -->
     <link
@@ -85,7 +114,7 @@
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                   <img
-                    src="assets/dist/img/user2-160x160.jpg"
+                    src="dist/img/user2-160x160.jpg"
                     class="user-image"
                     alt="User Image"
                   />
@@ -105,7 +134,7 @@
           <div class="user-panel">
             <div class="pull-left image">
               <img
-                src="assets/dist/img/user2-160x160.jpg"
+                src="dist/img/user2-160x160.jpg"
                 class="img-circle"
                 alt="User Image"
               />
@@ -165,18 +194,39 @@
     <!-- ./wrapper -->
 
     <!-- jQuery 3 -->
-    <script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src="assets/bower_components/jquery-ui/jquery-ui.min.js"></script>
+    <script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 
+    <!-- Bootstrap 3.3.7 -->
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- Morris.js charts -->
+    <script src="bower_components/raphael/raphael.min.js"></script>
+    <script src="bower_components/morris.js/morris.min.js"></script>
+    <!-- Sparkline -->
+    <script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+    <!-- jvectormap -->
+    <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+    <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
     <!-- daterangepicker -->
-    <script src="assets/bower_components/moment/min/moment.min.js"></script>
-    <script src="assets/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <script src="bower_components/moment/min/moment.min.js"></script>
+    <script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
     <!-- datepicker -->
-    <script src="assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+    <script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
     <!-- Bootstrap WYSIHTML5 -->
-    <script src="assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+    <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
     <!-- Slimscroll -->
-    <script src="assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <!-- FastClick -->
+    <script src="bower_components/fastclick/lib/fastclick.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="dist/js/pages/dashboard.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="dist/js/demo.js"></script>
   </body>
 </html>
