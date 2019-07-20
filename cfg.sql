ee06e1f18cc8f88822167a0a8de049add5057e6b
N7Aakash <t2lassi12345@gmail.com>
2019-07-20 19:56:46 +0530

    changed schema

---
+++ cfg.sql
@@ -0,0 +1,659 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 20, 2019 at 04:18 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cfg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity`
+--
+
+CREATE TABLE `activity` (
+  `activity_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `theme_id` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `media` text NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity_programme`
+--
+
+CREATE TABLE `activity_programme` (
+  `activity_program_id` int(11) NOT NULL,
+  `activity_id` int(11) NOT NULL,
+  `programme_id` int(11) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assessment`
+--
+
+CREATE TABLE `assessment` (
+  `assessment_id` int(11) NOT NULL,
+  `theme_id` int(11) NOT NULL,
+  `mentor_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `parameter_id` int(11) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assessment_parameter_criteria`
+--
+
+CREATE TABLE `assessment_parameter_criteria` (
+  `assessment_parameter_criteria_id` int(11) NOT NULL,
+  `parameter_id` int(11) NOT NULL,
+  `parameter_criteria_name` varchar(255) NOT NULL,
+  `parameter_criteria_value` int(11) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `attendance_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `isPresent` int(11) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mentor`
+--
+
+CREATE TABLE `mentor` (
+  `mentor_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `prog_id` int(11) NOT NULL,
+  `batch` varchar(255) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mentor_programme`
+--
+
+CREATE TABLE `mentor_programme` (
+  `mentor_programme_id` int(11) NOT NULL,
+  `mentor_id` int(11) NOT NULL,
+  `programme_id` int(11) NOT NULL,
+  `batch` varchar(255) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parameters`
+--
+
+CREATE TABLE `parameters` (
+  `parameter_id` int(11) NOT NULL,
+  `parameter_name` varchar(255) NOT NULL,
+  `mentor_id` int(11) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `programme`
+--
+
+CREATE TABLE `programme` (
+  `programme_id` int(11) NOT NULL,
+  `programme_name` varchar(255) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `deleted_by` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `programme`
+--
+
+INSERT INTO `programme` (`programme_id`, `programme_name`, `created_on`, `created_by`, `deleted_by`, `deleted_on`, `is_deleted`, `updated_by`, `updated_on`) VALUES
+(1, 'BEGINNER', '2019-07-20 18:00:28', 1, 0, '0000-00-00 00:00:00', 0, 0, '0000-00-00 00:00:00'),
+(2, 'FOUNDATION', '2019-07-20 18:00:28', 1, 0, '0000-00-00 00:00:00', 0, 0, '0000-00-00 00:00:00'),
+(3, 'TRANSIT', '2019-07-20 18:00:28', 1, 0, '0000-00-00 00:00:00', 0, 0, '0000-00-00 00:00:00'),
+(4, 'DISCOVERY', '2019-07-20 18:00:28', 1, 0, '0000-00-00 00:00:00', 0, 0, '0000-00-00 00:00:00'),
+(5, 'DREAM', '2019-07-20 18:00:28', 1, 0, '0000-00-00 00:00:00', 0, 0, '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `savings`
+--
+
+CREATE TABLE `savings` (
+  `savings_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount` varchar(255) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_iby` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `savings_isdue`
+--
+
+CREATE TABLE `savings_isdue` (
+  `savings_isdue_id` int(11) NOT NULL,
+  `savings_id` int(11) NOT NULL,
+  `month_amount` varchar(255) NOT NULL,
+  `is_due` int(2) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(2) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `starchart`
+--
+
+CREATE TABLE `starchart` (
+  `starchart_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `mentor_id` int(11) NOT NULL,
+  `param_criteria_id` int(11) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `starchart_parameter`
+--
+
+CREATE TABLE `starchart_parameter` (
+  `starchart_parameter_id` int(11) NOT NULL,
+  `starchart_parameter_name` varchar(255) NOT NULL,
+  `mentor_id` int(11) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `starchart_parameter_criteria`
+--
+
+CREATE TABLE `starchart_parameter_criteria` (
+  `starchart_parameter_criteria_id` int(11) NOT NULL,
+  `starchart_parameter_id` int(11) NOT NULL,
+  `starchart_parameter_criteria_name` varchar(255) NOT NULL,
+  `starchart_parameter_criteria_value` varchar(10) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(2) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `student_id` int(11) NOT NULL,
+  `student_first_name` varchar(255) NOT NULL,
+  `student_last_name` varchar(255) NOT NULL,
+  `student_school` varchar(255) NOT NULL,
+  `student_batch` varchar(255) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(2) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_programme`
+--
+
+CREATE TABLE `student_programme` (
+  `student_programme_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `programme_id` int(11) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  `is_deleted` int(2) NOT NULL,
+  `deleted_by` datetime NOT NULL,
+  `deleted_at` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `theme`
+--
+
+CREATE TABLE `theme` (
+  `theme_id` int(11) NOT NULL,
+  `theme_name` varchar(255) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_first_name` varchar(255) NOT NULL,
+  `user_last_name` varchar(255) NOT NULL,
+  `user_gender` varchar(10) NOT NULL,
+  `user_phone` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_password` text NOT NULL,
+  `user_role_id` int(11) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_by` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL,
+  `deleted_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role_type`
+--
+
+CREATE TABLE `user_role_type` (
+  `user_role_type_id` int(11) NOT NULL,
+  `user_role_type_name` varchar(255) NOT NULL,
+  `user_hierarchy` int(11) NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_by` int(11) NOT NULL,
+  `updated_on` datetime NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `deleted_by` int(11) NOT NULL,
+  `deleted_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_role_type`
+--
+
+INSERT INTO `user_role_type` (`user_role_type_id`, `user_role_type_name`, `user_hierarchy`, `created_by`, `created_on`, `updated_by`, `updated_on`, `is_deleted`, `deleted_by`, `deleted_on`) VALUES
+(1, 'ADMIN', 1, 0, '2019-07-20 16:51:58', 0, '0000-00-00 00:00:00', 0, 0, '0000-00-00 00:00:00'),
+(2, 'MENTOR', 2, 1, '2019-07-20 16:51:58', 0, '0000-00-00 00:00:00', 0, 0, '0000-00-00 00:00:00'),
+(3, 'VOLUNTEER', 3, 1, '2019-07-20 16:54:12', 0, '0000-00-00 00:00:00', 0, 0, '0000-00-00 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`activity_id`);
+
+--
+-- Indexes for table `activity_programme`
+--
+ALTER TABLE `activity_programme`
+  ADD PRIMARY KEY (`activity_program_id`);
+
+--
+-- Indexes for table `assessment`
+--
+ALTER TABLE `assessment`
+  ADD PRIMARY KEY (`assessment_id`);
+
+--
+-- Indexes for table `assessment_parameter_criteria`
+--
+ALTER TABLE `assessment_parameter_criteria`
+  ADD PRIMARY KEY (`assessment_parameter_criteria_id`);
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`attendance_id`);
+
+--
+-- Indexes for table `mentor`
+--
+ALTER TABLE `mentor`
+  ADD PRIMARY KEY (`mentor_id`);
+
+--
+-- Indexes for table `mentor_programme`
+--
+ALTER TABLE `mentor_programme`
+  ADD PRIMARY KEY (`mentor_programme_id`);
+
+--
+-- Indexes for table `parameters`
+--
+ALTER TABLE `parameters`
+  ADD PRIMARY KEY (`parameter_id`);
+
+--
+-- Indexes for table `programme`
+--
+ALTER TABLE `programme`
+  ADD PRIMARY KEY (`programme_id`);
+
+--
+-- Indexes for table `savings`
+--
+ALTER TABLE `savings`
+  ADD PRIMARY KEY (`savings_id`);
+
+--
+-- Indexes for table `savings_isdue`
+--
+ALTER TABLE `savings_isdue`
+  ADD PRIMARY KEY (`savings_isdue_id`);
+
+--
+-- Indexes for table `starchart`
+--
+ALTER TABLE `starchart`
+  ADD PRIMARY KEY (`starchart_id`);
+
+--
+-- Indexes for table `starchart_parameter`
+--
+ALTER TABLE `starchart_parameter`
+  ADD PRIMARY KEY (`starchart_parameter_id`);
+
+--
+-- Indexes for table `starchart_parameter_criteria`
+--
+ALTER TABLE `starchart_parameter_criteria`
+  ADD PRIMARY KEY (`starchart_parameter_criteria_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `student_programme`
+--
+ALTER TABLE `student_programme`
+  ADD PRIMARY KEY (`student_programme_id`);
+
+--
+-- Indexes for table `theme`
+--
+ALTER TABLE `theme`
+  ADD PRIMARY KEY (`theme_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_role_type`
+--
+ALTER TABLE `user_role_type`
+  ADD PRIMARY KEY (`user_role_type_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activity`
+--
+ALTER TABLE `activity`
+  MODIFY `activity_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `activity_programme`
+--
+ALTER TABLE `activity_programme`
+  MODIFY `activity_program_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `assessment`
+--
+ALTER TABLE `assessment`
+  MODIFY `assessment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `assessment_parameter_criteria`
+--
+ALTER TABLE `assessment_parameter_criteria`
+  MODIFY `assessment_parameter_criteria_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mentor`
+--
+ALTER TABLE `mentor`
+  MODIFY `mentor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mentor_programme`
+--
+ALTER TABLE `mentor_programme`
+  MODIFY `mentor_programme_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `parameters`
+--
+ALTER TABLE `parameters`
+  MODIFY `parameter_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `programme`
+--
+ALTER TABLE `programme`
+  MODIFY `programme_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `savings`
+--
+ALTER TABLE `savings`
+  MODIFY `savings_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `savings_isdue`
+--
+ALTER TABLE `savings_isdue`
+  MODIFY `savings_isdue_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `starchart`
+--
+ALTER TABLE `starchart`
+  MODIFY `starchart_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `starchart_parameter`
+--
+ALTER TABLE `starchart_parameter`
+  MODIFY `starchart_parameter_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `starchart_parameter_criteria`
+--
+ALTER TABLE `starchart_parameter_criteria`
+  MODIFY `starchart_parameter_criteria_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_programme`
+--
+ALTER TABLE `student_programme`
+  MODIFY `student_programme_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `theme`
+--
+ALTER TABLE `theme`
+  MODIFY `theme_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_role_type`
+--
+ALTER TABLE `user_role_type`
+  MODIFY `user_role_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
